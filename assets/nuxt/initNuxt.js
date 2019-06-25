87d513553c0154e7bd305f1cf59d96b8cb2d1b95
karimcossutti <karim.cossutti@drunomics.com>
2019-06-25 17:31:05 +0200

    Fix query selector function

--- assets/nuxt/initNuxt.js
+++ assets/nuxt/initNuxt.js
@@ -1,4 +1,4 @@
-var contentEl = document.querySelectorAll('main[role="main"]');
+var contentEl = document.querySelector('main[role="main"]');
 var content = contentEl.innerHTML || '';
 
 if (Object.keys(window.__NUXT__.data[0]).length === 0 ) {
