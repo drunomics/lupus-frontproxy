ee1d9983c30ad3e756cf810d9a93795e4cc60809
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2020-05-02 22:06:01 +0200

    Improve lupus.initialState generate and drop object assign polyfill.

--- assets/nuxt/initNuxt.js
+++ assets/nuxt/initNuxt.js
@@ -1,5 +1,3 @@
-/** Object assign polyfill */
-Object.assign||Object.defineProperty(Object,"assign",{enumerable:!1,configurable:!0,writable:!0,value:function(e){"use strict";if(null==e)throw new TypeError("Cannot convert first argument to object");for(var r=Object(e),t=1;t<arguments.length;t++){var n=arguments[t];if(null!=n){n=Object(n);for(var c=Object.keys(Object(n)),o=0,a=c.length;o<a;o++){var b=c[o],i=Object.getOwnPropertyDescriptor(n,b);void 0!==i&&i.enumerable&&(r[b]=n[b])}}}return r}});
 
 window.lupusContentEl = document.querySelector('main[role="main"]');
 window.lupus.initialState.content = window.lupusContentEl ? window.lupusContentEl.innerHTML : '';
