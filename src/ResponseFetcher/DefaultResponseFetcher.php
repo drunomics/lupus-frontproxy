a30682405b4a5a6ce856da2d8f4e7ff0f0e739d7
Petar <petar.basic@drunomics.com>
2020-06-22 16:02:38 +0200

    WV-3839: Add new local frontend response fetcher.

--- src/ResponseFetcher/DefaultResponseFetcher.php
+++ src/ResponseFetcher/DefaultResponseFetcher.php
@@ -3,7 +3,6 @@
 namespace drunomics\LupusFrontProxy\ResponseFetcher;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\RequestInterface;
 
 /**
@@ -40,23 +39,13 @@ class DefaultResponseFetcher implements ResponseFetcherInterface {
       'allow_redirects' => FALSE,
       'http_errors' => FALSE,
     ]);
-    // Try fetching static html via filesystem
-    $root = getenv('PWD');
-    $site = getenv('SITE');
-    $request_body = file_get_contents($root . '/frontend/public/' . $site . '/layout--default.html');
-    if ($request_body) {
-      $frontend_response = new Response(200,[],$request_body);
-    }
-    else {
-      // Fallback to letting server handle the request
-      // $frontend_response = $client->sendAsync($frontend_request);
-      // Resolve the frontend promise first since static files should be always
-      // faster than the backend.
-      /** @var \Psr\Http\Message\ResponseInterface $frontend_response */
-      // $frontend_response = $frontend_response->wait();
-    }
+    $frontend_response = $client->sendAsync($frontend_request);
 
+    // Resolve the frontend promise first since static files should be always
+    // faster than the backend.
+    /** @var \Psr\Http\Message\ResponseInterface $frontend_response */
     /** @var \Psr\Http\Message\ResponseInterface $backend_response */
+    $frontend_response = $frontend_response->wait();
     $backend_response = $backend_response->wait();
     return [$frontend_response, $backend_response];
   }
